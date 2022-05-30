f72f5f8a7fb75bf91901ed147beaadb5c0bea707
Davi Belo <davibelocontato@gmail.com>
2022-05-30 17:39:13 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let tophead = ' ______';
+let sidehead = '| ** |';
+let nose = '| ^  |';
+let mouth = '| -- |'
+let phrase = '->Hello Random, I am Davi, I do not know if you saw, but I lost my right arm, can you help me to build a new arm with JavaScript?--'
+let bottom = '------';
+let neck = '  ||'
+let body = '|    |'
+let arm = '--------'
+let endarm = '|'
+let bodybottom = '------    '
+let legs = '||  ||'
+
+console.log(tophead,'\n',sidehead,'\n',nose,'\n',mouth,phrase,'\n',bottom,'\n',neck,'\n',body,'\n',body,arm,'\n',body,arm,'\n',body,'\n',bodybottom,'\n',legs,'\n',legs,'\n',legs);
+
+
+
